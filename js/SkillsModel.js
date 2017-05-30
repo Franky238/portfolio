a96e0a5ca9bbb1d99176b7634b675789349b8edd
Frantisek Ferko <frank238238@gmail.com>
2017-05-30 06:09:25 +0200

    Updated skills

--- js/SkillsModel.js
+++ js/SkillsModel.js
@@ -5,7 +5,7 @@ var SkillsModel = {
             css: '85%',
             js: '82%',
             php: '73%',
-            typescript: '45%',
+            typescript: '72%',
             sql: '67%',
             python: '38%'
         };
