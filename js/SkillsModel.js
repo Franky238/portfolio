16fbd9bfd2e3155a31c4c6c08ae32436d2f20abc
Frantisek Ferko <frank238238@gmail.com>
2017-09-19 22:52:05 +0200

    Regular skills and cv update + version update

--- js/SkillsModel.js
+++ js/SkillsModel.js
@@ -3,9 +3,9 @@ var SkillsModel = {
         var skills = {
             html: '90%',
             css: '85%',
-            js: '82%',
+            js: '84%',
             php: '73%',
-            typescript: '72%',
+            typescript: '79%',
             sql: '67%',
             python: '38%'
         };
